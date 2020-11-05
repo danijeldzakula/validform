ad23c0c39f906a608615a861cce044c82793002f
Dado19Git <danidzaki060@gmail.com>
2020-11-05 14:22:06 +0100

    mobile first

--- app.js
+++ app.js
@@ -9,16 +9,19 @@ const formMessage = doc.querySelector('.form-message');
 
 
 formCharacter.forEach((el, index, node) => {
+
 	el.addEventListener('change', (e) => {
 		e.preventDefault();
 
 		let patternCharacter = /^[a-z šđčćž]+$/gi;
 
+		console.log(e)
+
 		if(e.target.value.length >= 3 && e.target.value.match(patternCharacter)) {
 			console.log('Valid name length and character type');
 			return true;
 		} else {
-			console.log('Invalid name length or type character');
+			console.log('Invalid name length or character type');
 			e.target.value = '';
 			return false;
 		}
@@ -35,11 +38,13 @@ formNumber.forEach((el, index, node) => {
 
 		let patternNumber = /^[0-9 -\/]+$/gi;
 
+		console.log(e)
+
 		if(e.target.value.length >= 10 && e.target.value.match(patternNumber)) {
 			console.log('Valid number length and type');
 			return true;
 		} else {
-			console.log('Invalid number length or type character');
+			console.log('Invalid number length or character type');
 			e.target.value = '';
 			return false;
 		}
@@ -54,9 +59,18 @@ function validateCharacter(e) {
     return patternCharacter.test(e.key);
 }
 
+function validateCharacterMobile(e) {
+    e.target.value = e.target.value.replace(/^[0-9 -\/]+$/gi,'');
+    return false;
+}
+
 function validateNumber(e) {
     let patternNumber = /^[0-9 -\/]+$/gi;
     return patternNumber.test(e.key);
 }
 
+function validateNumberMobile(e) {
+    e.target.value = e.target.value.replace(/^[0-9 -\/]+$/gi,'');
+    return false;
+}
 
